169a8c36f43ce517c9011b67a3702ef7e6fe58cd
AhmadRam <a.n.s.r.96@gmail.com>
2024-05-27 19:35:30 +0300

    a

--- packages/Webkul/Admin/src/Resources/views/sales/address.blade.php
+++ packages/Webkul/Admin/src/Resources/views/sales/address.blade.php
@@ -6,9 +6,14 @@
     <p class="text-gray-800 font-semibold leading-6 dark:text-white">
         {{ $address->name }}
     </p>
-    
+
     <p class="text-gray-600 dark:text-gray-300 !leading-6">
-        {{ $address->address1 }}<br>
+        <?php $address1 = explode(PHP_EOL, $address->address1); ?>
+
+        {{ trans('shop::app.checkout.onepage.addresses.billing.block-address') }} : {{ $address1[0] }}<br>
+        {{ trans('shop::app.checkout.onepage.addresses.billing.street-address') }} : {{ $address1[1] }}<br>
+        {{ trans('shop::app.checkout.onepage.addresses.billing.floor-address') }} : {{ $address1[2] }}<br>
+        {{ trans('shop::app.checkout.onepage.addresses.billing.house-address') }} : {{ $address1[3] }}<br>
 
         @if ($address->address2)
             {{ $address->address2 }}<br>
@@ -18,7 +23,10 @@
 
         {{ $address->state }}<br>
 
-        {{ core()->country_name($address->country) }} @if ($address->postcode) ({{ $address->postcode }}) @endif<br>
+        {{ core()->country_name($address->country) }} @if ($address->postcode)
+            ({{ $address->postcode }})
+        @endif
+        <br>
 
         {{ __('admin::app.sales.orders.view.contact') }} : {{ $address->phone }}
     </p>
