b6756d1f860c54e3d220fd2b3651e47167a73b8f
AhmadRam <a.n.s.r.96@gmail.com>
2024-05-30 21:04:38 +0300

    a

--- config/app.php
+++ config/app.php
@@ -226,6 +226,7 @@ return [
         Webkul\Theme\Providers\ThemeServiceProvider::class,
         Webkul\User\Providers\UserServiceProvider::class,
         Webkul\Myfatoorah\Providers\MyfatoorahServiceProvider::class,
+        Jenssegers\Agent\AgentServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -251,5 +252,6 @@ return [
         'ProductImage' => Webkul\Product\Facades\ProductImage::class,
         'ProductVideo' => Webkul\Product\Facades\ProductVideo::class,
         'Redis'        => Illuminate\Support\Facades\Redis::class,
+        'Agent'        => Jenssegers\Agent\Facades\Agent::class,
     ])->toArray(),
 ];
