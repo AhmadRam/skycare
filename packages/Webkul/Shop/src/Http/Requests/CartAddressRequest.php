c33faf1412451c382b34df9d73494bf97f0c008c
AhmadRam <a.n.s.r.96@gmail.com>
2024-06-06 11:54:42 +0300

    a

--- packages/Webkul/Shop/src/Http/Requests/CartAddressRequest.php
+++ packages/Webkul/Shop/src/Http/Requests/CartAddressRequest.php
@@ -55,14 +55,14 @@ class CartAddressRequest extends FormRequest
         }
 
         $this->mergeWithRules([
-            "{$addressType}.first_name" => ['required', new AlphaNumericSpace],
-            "{$addressType}.last_name"  => ['required', new AlphaNumericSpace],
+            "{$addressType}.first_name" => ['required'],
+            "{$addressType}.last_name"  => ['required'],
             "{$addressType}.email"      => ['required'],
             "{$addressType}.address1"   => ['required', 'array', 'min:1'],
             "{$addressType}.address1.*" => ['string'],
             "{$addressType}.city"       => ['required'],
-            "{$addressType}.country"    => [new AlphaNumericSpace],
-            "{$addressType}.state"      => [new AlphaNumericSpace],
+            "{$addressType}.country"    => ['required'],
+            "{$addressType}.state"      => ['required'],
             "{$addressType}.postcode"   => ['numeric'],
             "{$addressType}.phone"      => ['required', new PhoneNumber],
         ]);
