70a28b24feec2fa64ba0fbba9df906e4b6766094
AhmadRam <a.n.s.r.96@gmail.com>
2024-06-11 16:04:45 +0300

    a

--- packages/Webkul/Shop/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/index.blade.php
@@ -95,6 +95,20 @@
     </script>
     <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            setTimeout(function() {
+                if ($('#exampleModal').length) {
+                    $('#exampleModal').modal('show');
+                }
+            }, 2000);
+        });
+    </script>
+
 </body>
 
 </html>
