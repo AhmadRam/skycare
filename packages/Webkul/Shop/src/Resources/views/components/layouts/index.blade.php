cf0f926fa38f0e1c5cb9965d2e722340de83aef1
AhmadRam <a.n.s.r.96@gmail.com>
2024-09-01 13:29:15 +0300

    a

--- packages/Webkul/Shop/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/index.blade.php
@@ -69,9 +69,34 @@
     <noscript><img height="1" width="1" style="display:none"
             src="https://www.facebook.com/tr?id=1051569896344943&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-MMHJVPMM');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MMHJVPMM" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     {!! view_render_event('bagisto.shop.layout.body.before') !!}
 
     <a href="#main" class="skip-to-main-content-link">Skip to main content</a>
