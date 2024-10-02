490b18382ee4187785ae4844f5dc885e4e86966b
AhmadRam <a.n.s.r.96@gmail.com>
2024-10-02 10:13:03 +0300

    a

--- packages/Webkul/Sales/src/Jobs/SendNotificationToAbandonedCarts.php
+++ packages/Webkul/Sales/src/Jobs/SendNotificationToAbandonedCarts.php
@@ -25,7 +25,7 @@ class SendNotificationToAbandonedCarts implements ShouldQueue
     public function handle()
     {
         $date = Carbon::now()->subHours(3);
-        $carts = app(CartRepository::class)->whereNotNull('customer_email')->where('created_at', '>=', $date)->get();
+        $carts = app(CartRepository::class)->whereNotNull('customer_email')->where('created_at', '>=', $date)->where('is_active', 0)->get();
         foreach ($carts as $cart) {
             Mail::queue(new AbandonedCart($cart));
         }
